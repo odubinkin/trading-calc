f62c1f93f9abffea0db67b37f38100b378a8f9d0
Oleg Dubinkin <odubinkin@gmail.com>
2024-08-30 14:51:08 +0700

    change TP calculation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 import { useState, useEffect } from 'react';
 import { Box, FormControl, FormLabel, NumberInput, NumberInputField, VStack, Text, HStack, Divider } from '@chakra-ui/react';
 
+const DECIMALS = 3;
+const DECIMALS_PERCENTS = 2;
+
 // Function to calculate the DCA (Dollar-Cost Averaging) price
 const calculateDcaPrice = (entryPrices: number[]) => {
     return entryPrices.reduce((a, b) => a + b, 0) / entryPrices.length;
@@ -12,14 +15,12 @@ const calculateSlPrice = (dcaPrice: number, slPercentage: number) => {
 };
 
 // Function to calculate the Take Profit (TP) prices
-const calculateTpPrices = (dcaPrice: number, numOrders: number, tpPercentage: number, tpFearFactor: number, tpGreedFactor: number) => {
+const calculateTpPrices = (dcaPrice: number, numOrders: number, minTpPercentage: number, maxTpPercentage: number) => {
     const tpPrices = [];
-    const averageFactor = (tpFearFactor + tpGreedFactor) / 2;
-    const adjustmentFactor = tpPercentage / (averageFactor * 100);
+    const step = (maxTpPercentage - minTpPercentage) / (numOrders - 1);
     for (let i = 0; i < numOrders; i++) {
-        const factor = tpFearFactor + (tpGreedFactor - tpFearFactor) * (i / (numOrders - 1));
-        const tpAdjustedPercentage = adjustmentFactor * factor;
-        tpPrices.push(dcaPrice * (1 + tpAdjustedPercentage));
+        const tpPercentage = minTpPercentage + i * step;
+        tpPrices.push(dcaPrice * (1 + tpPercentage / 100));
     }
     return tpPrices;
 };
@@ -45,9 +46,8 @@ const TradingCalc = () => {
     const [lowerPrice, setLowerPrice] = useState<number>(0);
     const [numOrders, setNumOrders] = useState<number>(5);
     const [slPercentage, setSlPercentage] = useState<number>(5);
-    const [tpPercentage, setTpPercentage] = useState<number>(5);
-    const [tpFearFactor, setTpFearFactor] = useState<number>(0.4);
-    const [tpGreedFactor, setTpGreedFactor] = useState<number>(1.8);
+    const [minTpPercentage, setMinTpPercentage] = useState<number>(1.5);
+    const [maxTpPercentage, setMaxTpPercentage] = useState<number>(8.5);
 
     const [entryPrices, setEntryPrices] = useState<number[]>([]);
     const [dcaPrice, setDcaPrice] = useState<number>(0);
@@ -65,11 +65,11 @@ const TradingCalc = () => {
         const dca = calculateDcaPrice(prices);
         setDcaPrice(dca);
         setSlPrice(calculateSlPrice(dca, slPercentage));
-        const tp = calculateTpPrices(dca, numOrders, tpPercentage, tpFearFactor, tpGreedFactor);
+        const tp = calculateTpPrices(dca, numOrders, minTpPercentage, maxTpPercentage);
         setTpPrices(tp);
-        setPercentageDiffs(calculatePercentageDiffs(prices, dca));
         setAverageTp(calculateAverageTp(tp));
-    }, [upperPrice, lowerPrice, numOrders, slPercentage, tpPercentage, tpFearFactor, tpGreedFactor]);
+        setPercentageDiffs(calculatePercentageDiffs(prices, dca));
+    }, [upperPrice, lowerPrice, numOrders, slPercentage, minTpPercentage, maxTpPercentage, averageTp]);
 
     return (
         <Box p={5}>
@@ -100,35 +100,29 @@ const TradingCalc = () => {
                         </NumberInput>
                     </FormControl>
                     <FormControl>
-                        <FormLabel>Take Profit Percentage from DCA</FormLabel>
-                        <NumberInput value={tpPercentage} onChange={(valueString) => setTpPercentage(parseFloat(valueString))} step={0.01} precision={2}>
+                        <FormLabel>Minimum Take Profit Percentage from DCA</FormLabel>
+                        <NumberInput value={minTpPercentage} onChange={(valueString) => setMinTpPercentage(parseFloat(valueString))} step={0.01} precision={2}>
                             <NumberInputField />
                         </NumberInput>
                     </FormControl>
                     <FormControl>
-                        <FormLabel>Fear Factor for TP</FormLabel>
-                        <NumberInput value={tpFearFactor} onChange={(valueString) => setTpFearFactor(parseFloat(valueString))} step={0.01} precision={2}>
-                            <NumberInputField />
-                        </NumberInput>
-                    </FormControl>
-                    <FormControl>
-                        <FormLabel>Greed Factor for TP</FormLabel>
-                        <NumberInput value={tpGreedFactor} onChange={(valueString) => setTpGreedFactor(parseFloat(valueString))} step={0.01} precision={2}>
+                        <FormLabel>Maximum Take Profit Percentage from DCA</FormLabel>
+                        <NumberInput value={maxTpPercentage} onChange={(valueString) => setMaxTpPercentage(parseFloat(valueString))} step={0.01} precision={2}>
                             <NumberInputField />
                         </NumberInput>
                     </FormControl>
                 </VStack>
                 <Divider orientation="vertical" />
                 <VStack spacing={4} width="60%" align="start">
-                    <Text>Average DCA Price: {dcaPrice}</Text>
-                    <Text>Stop Loss (SL) Level: {slPrice}</Text>
-                    <Text>Average TP Value: {averageTp}</Text>
+                    <Text>Average DCA Price: {dcaPrice.toFixed(DECIMALS)}</Text>
+                    <Text>Average TP Value: {averageTp.toFixed(DECIMALS)} ({((averageTp-dcaPrice)*100/dcaPrice).toFixed(DECIMALS_PERCENTS)}%)</Text>
+                    <Text>Stop Loss (SL) Level: {slPrice.toFixed(DECIMALS)}</Text>
                     {entryPrices.map((price, index) => (
                         <Box key={index} p={2} borderWidth="1px" borderRadius="lg" width="100%">
                             <Text>Order {index + 1}:</Text>
-                            <Text>Entry Price: {price}</Text>
-                            <Text>Deviation from DCA: {percentageDiffs[index].toFixed(2)}%</Text>
-                            <Text>TP Level: {tpPrices[index]}</Text>
+                            <Text>Entry Price: {price.toFixed(DECIMALS)}</Text>
+                            <Text>Deviation from DCA: {percentageDiffs[index].toFixed(DECIMALS_PERCENTS)}%</Text>
+                            <Text>TP Level: {tpPrices[index].toFixed(DECIMALS)}</Text>
                         </Box>
                     ))}
                 </VStack>
