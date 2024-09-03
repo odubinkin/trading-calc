05b6776d6224c6cc8a0db217f95241b7c5e77711
Oleg Dubinkin <odubinkin@gmail.com>
2024-09-03 18:55:57 +0700

    update deps

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 
 // Mock matchMedia
